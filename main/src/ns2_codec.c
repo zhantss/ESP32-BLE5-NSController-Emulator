f07444b79446db560747bc03e60c6899ec9f10e9
Tss <zhantss@hotmail.com>
2026-04-16 11:25:06 +0800

    update sdk config

--- main/src/ns2_codec.c
+++ main/src/ns2_codec.c
@@ -489,7 +489,7 @@ static uint8_t cmd_0x15_handler(const uint8_t subcmd, const uint16_t payload_len
         // paring finished
         case 0x03:
             // TODO 00 ? before index 9 ? WHY 8?
-            if (data_in[8] == 0x00) {
+            if (data_in[8] == 0x00 || data_in[9] == 0x00) {
                 rc = controller_pairing_info_save();
                 rc += inject_pairing_info_to_ble_ctx();
 
