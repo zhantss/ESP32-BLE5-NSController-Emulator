52584d1cbaedc05d855f911d8712e18d0a716f8a
Tss <zhantss@hotmail.com>
2026-04-25 14:41:28 +0800

    Use SOC_AES_SUPPORTED to determine whether to use the esp_aes API instead of mbedtls AES.

--- main/src/utils.c
+++ main/src/utils.c
@@ -1,6 +1,10 @@
 #include "utils.h"
 
+#ifndef CONFIG_SOC_AES_SUPPORTED
+#include "mbedtls/aes.h"
+#else
 #include "aes/esp_aes.h"
+#endif
 
 void reverse_bytes(const uint8_t* in, uint8_t* out, size_t len) {
   for (size_t i = 0; i < len; i++) {
@@ -8,6 +12,25 @@ void reverse_bytes(const uint8_t* in, uint8_t* out, size_t len) {
   }
 }
 
+#ifndef CONFIG_SOC_AES_SUPPORTED
+static int aes128_ecb_pri(mbedtls_aes_context aes_ctx, uint8_t* key, uint8_t* in, uint8_t* out) {
+  int rc = 0;
+  rc = mbedtls_aes_setkey_enc(&aes_ctx, key, 128);
+  if (rc != 0) {
+    return rc;
+  }
+  return mbedtls_aes_crypt_ecb(&aes_ctx, MBEDTLS_AES_ENCRYPT, in, out);
+}
+
+int aes128_ecb(uint8_t* key, uint8_t* in, uint8_t* out) {
+  mbedtls_aes_context aes_ctx;
+  int rc = 0;
+  mbedtls_aes_init(&aes_ctx);
+  rc = aes128_ecb_pri(aes_ctx, key, in, out);
+  mbedtls_aes_free(&aes_ctx);
+  return rc;
+}
+#else
 static int aes128_ecb_pri(esp_aes_context aes_ctx, uint8_t* key, uint8_t* in, uint8_t* out) {
   int rc = 0;
   rc = esp_aes_setkey(&aes_ctx, key, 128);
@@ -25,6 +48,7 @@ int aes128_ecb(uint8_t* key, uint8_t* in, uint8_t* out) {
   esp_aes_free(&aes_ctx);
   return rc;
 }
+#endif
 
 uint8_t peek_byte(uint8_t *head, uint32_t head_len,
                   uint8_t *wrap, uint32_t wrap_len,
